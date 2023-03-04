42d0bd43aad83676a8d5ade39e5312986594ebb1
Doston04 <avazovjr@gmail.com>
2023-03-04 17:54:05 +0500

    mana stats

--- components/main/whyderek.tsx
+++ components/main/whyderek.tsx
@@ -4,48 +4,58 @@ import styles from "../../styles/home.module.css";
 import { MotionSection } from "./motion-section";
 import mainImage from "../../public/media/fade_img.png";
 import { kachok, mikrob, qalqon, tish } from "../../public/icons";
+import { useContext } from "react";
+import { TranslationsContext } from "../../context/translations";
+import img6 from "../../public/media/img6.png";
+import img5 from "../../public/media/img5.png";
+import img4 from "../../public/media/img4.png";
+import img3 from "../../public/media/img3.png";
+import img2 from "../../public/media/img2.png";
+import img1 from "../../public/media/img1.png";
 
 export function MainWhyDerek() {
   const { ref: section, inView: sectionIsVisible } = useInView({
     triggerOnce: true,
   });
 
+  const { t } = useContext(TranslationsContext);
+
   const gridCards = [
     {
       id: 1,
-      title: "Uzoq vaqt davomida turishi",
-      desc: "xt ever since the 1500s, when an unknown printer took a ",
-      icon: kachok,
+      title: t["stats.stat1_title"],
+      desc: t["stats.stat1_desc"],
+      icon: img1,
     },
     {
       id: 2,
-      title: "Kosmetik tarafdan chiroyli",
-      desc: "xt ever since the 1500s, when an unknown printer took a ",
-      icon: tish,
+      title: t["stats.stat2_title"],
+      desc: t["stats.stat2_desc"],
+      icon: img2,
     },
     {
       id: 3,
-      title: "Anti alergik hususiyati",
-      desc: "xt ever since the 1500s, when an unknown printer took a ",
-      icon: mikrob,
+      title: t["stats.stat3_title"],
+      desc: t["stats.stat3_desc"],
+      icon: img3,
     },
     {
       id: 4,
-      title: "Odatiy tishlarga nisbatan 2 barobar mustahkamroq",
-      desc: "xt ever since the 1500s, when an unknown printer took a ",
-      icon: qalqon,
+      title: t["stats.stat4_title"],
+      desc: t["stats.stat4_desc"],
+      icon: img4,
     },
     {
       id: 5,
-      title: "Odatiy tishlarga nisbatan 2 barobar mustahkamroq",
-      desc: "xt ever since the 1500s, when an unknown printer took a ",
-      icon: qalqon,
+      title: t["stats.stat5_title"],
+      desc: t["stats.stat5_desc"],
+      icon: img5,
     },
     {
       id: 6,
-      title: "Anti alergik hususiyati",
-      desc: "xt ever since the 1500s, when an unknown printer took a ",
-      icon: mikrob,
+      title: t["stats.stat6_title"],
+      desc: t["stats.stat6_desc"],
+      icon: img6,
     },
   ];
 
@@ -59,7 +69,7 @@ export function MainWhyDerek() {
         <div className={styles.content_side}>
           <div className={styles.content_titles}>
             <p>
-              <b>Nima uchun aynan DEREK?</b> #WeloveyourSMILE
+              <b>Nima uchun DEREK?</b> #WeloveyourSMILE
             </p>
             <p>
               xt ever since the 1500s, when an unknown printer took a galley of
@@ -76,7 +86,7 @@ export function MainWhyDerek() {
               return (
                 <div key={card.id} className={styles.content_grid_card}>
                   <span className={styles.content_grid_card_icon}>
-                    {card.icon}
+                    <Image src={card.icon} alt={card.title} />
                   </span>
                   <div className={styles.content_grid_card_info}>
                     <p className={styles.content_grid_card_title}>
